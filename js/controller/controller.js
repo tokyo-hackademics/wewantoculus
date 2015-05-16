9894de15f1f94cf89d5513820790256ff06807cb
onick-man <fumihiko.takahashi.222@gmail.com>
2015-05-16 16:45:03 +0900

    update controller

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,5 +1,8 @@
-//connect socket
+//connect OAOAAOAsocket
 var socket = io.connect(location.origin);
+
+// 加速度の閾値
+var accleThresh = 15.0;
 
 $(function(){
 	$("#main").click(function(){
@@ -8,14 +11,47 @@ $(function(){
 	});
 });
 
+function calcMerge(x,y){
+    return Math.sqrt(Math.pow(x,2) + Math.pow(y,2))
+}
+
+function calcEval(x,y,z){
+    return calcMerge(calcMerge(x,y),z);
+}
+
+function accelFlag(x){
+    if(x > accleThresh){ return true; }
+    return false;
+}
+
+var acF = false;
+
 window.addEventListener("devicemotion", function(event1){
     var x = event1.acceleration.x;
     var y = event1.acceleration.y;
     var z = event1.acceleration.z;
 
-    var result1 = document.getElementById("result");
+    var result1 = document.getElementById("result1");
+    var result2 = document.getElementById("result2");
+
     result1.innerHTML =
-        "X："+ Math.round(x * 10) / 10 +"<br>" +
-        "Y："+ Math.round(y * 10) / 10 +"<br>" +
-        "Z："+ Math.round(z * 10) / 10;
+        "xyz："+ calcEval(x,y,z) +"<br>";
+
+    result2.innerHTML =
+        calcEval(x,y,z);
+
+    if (accelFlag(x) || acF){
+        acF = true;
+        result2.innerHTML = "<div id='okBox'></div><br>";
+    }else{
+        result2.innerHTML = "<div id='badBox'></div><br>";
+    }
+
+
 }, true);
+
+
+function delete_form(frm){
+    frm.elements["id_btn"].style.display="none";
+    frm.elements["id_box"].style.display="none";
+}
