487de77ba26d8e05e8980923b11bb919e90fea80
onick-man <fumihiko.takahashi.222@gmail.com>
2015-05-17 15:25:05 +0900

    chg effects

--- js/index/effectanm.js
+++ js/index/effectanm.js
@@ -1,14 +1,15 @@
 function effectWater(){
 	$("#water").fadeIn(1000);
+  $("#water").animate({'bottom':'0px'},1000);
 	$("#water").fadeOut(1000);
 }
 
 function effectBomb(){
-	$("#bomb").fadeIn(1000);
+	$("#bomb").fadeIn(3000);
  	$('#bomb').jrumble({
         x: 2,
         y: 2,
-        rotation: 1
+        rotation: 4,
     });
     $("#bomb").trigger('startRumble');
     $("#bomb").fadeOut(1000);
