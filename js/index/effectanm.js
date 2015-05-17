50c77c94bf4704ff3f4dbd032f663ead8fa8393d
Yano <yano@noir.local>
2015-05-17 14:35:18 +0900

    effect function

--- js/index/effectanm.js
+++ js/index/effectanm.js
@@ -1,7 +1,15 @@
 function effectWater(){
-	
+	$("#water").fadeIn(1000);
+	$("#water").fadeOut(1000);
 }
 
 function effectBomb(){
-	
-}
+	$("#bomb").fadeIn(1000);
+ 	$('#bomb').jrumble({
+        x: 2,
+        y: 2,
+        rotation: 1
+    });
+    $("#bomb").trigger('startRumble');
+    $("#bomb").fadeOut(1000);
+ }
